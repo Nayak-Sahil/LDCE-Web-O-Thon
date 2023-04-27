3da25f2c076cfe7c04070d5e790330b0b9f4f443
Sahil Nayak <nayaksahil992003@gmail.com>
2023-04-27 16:10:40 +0530

    .

---
+++ script/script.js
@@ -0,0 +1,25 @@
+const eventDate = new Date("2023-05-15T09:00:00").getTime();
+
+        const timer = setInterval(function () {
+            const currentDate = new Date().getTime();
+
+            const timeDifference = eventDate - currentDate;
+
+            const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+            const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+            const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+            document.getElementById("days").innerHTML = days;
+            document.getElementById("hours").innerHTML = hours;
+            document.getElementById("minutes").innerHTML = minutes;
+            document.getElementById("seconds").innerHTML = seconds;
+            if (timeDifference < 0) {
+                clearInterval(timer);
+                document.getElementById("days").innerHTML = "0";
+                document.getElementById("hours").innerHTML = "0";
+                document.getElementById("minutes").innerHTML = "0";
+                document.getElementById("seconds").innerHTML = "0";
+                document.getElementById("countdown-timer").innerHTML = "Event has already started";
+            }
+        }, 1000);
